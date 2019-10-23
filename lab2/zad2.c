bbab3fb27d598a48d6d75ad3bbf8e743d5bb9ec6
Gacek <agry123@wp.pl>
2019-10-24 00:31:47 +0100

    ŁEE Tu jest NFTW w drugim pliku jest inne

--- lab2/zad2.c
+++ lab2/zad2.c
@@ -5,13 +5,22 @@
 #include <time.h>
 #include <dirent.h>
 #include <sys/stat.h>
-// ./test sciezka > data 
-const char format[] = "%d.%m.%Y %H:%M:%S";
+#include <linux/limits.h>
+//NFTW
+#include <ftw.h>
 
-void getFileInfo(char *path, struct stat *file){
+#include <sys/types.h>
+#include <unistd.h>
 
-		printf("File path\t%s", path);
-		printf("File size:\t%lld\n", file->st_size);
+// ./test sciezka > data
+const char format2[] = "%d-%m-%Y %H:%M";
+const char format[] = "%Y-%m-%d %H:%M:%S";
+const char *nftwoperant;
+time_t nftwDate;
+void getFileInfo(const char *path, const struct stat *file){
+
+		printf("\nFile path\t%s\n", path);
+		printf("File size:\t%ld\n", file->st_size);
 		printf("Permission: \t");
 		printf( (S_ISDIR(file->st_mode)) ? "d" : "-");
 		printf( (file->st_mode & S_IRUSR) ? "r" : "-");
@@ -23,92 +32,76 @@ void getFileInfo(char *path, struct stat *file){
 		printf( (file->st_mode & S_IROTH) ? "r" : "-");
 		printf( (file->st_mode & S_IWOTH) ? "w" : "-");
 		printf( (file->st_mode & S_IXOTH) ? "x" : "-");
-		printf("\n\n");
-		printf("Data of last modification /t%lld/n", ctime(&(file->st_mtime)));
-		
+		printf("\n");
+		printf("Data of last modification \t%s\n", ctime(&(file->st_mtime)));
+
 
 }
-void scanDIR(char *path, char sign, time_t date){
-	struct dirent * file;
-    DIR * folder;
-	struct stat buf;
-	char newPath[PATH_MAX];
-	if((folder = opendir(path))){
-		
-		while((file = readdir(folder)) != NULL){
-			  
-			  strcpy(newPath, path);
-			  strcat(newPath, "/");
-			  strcat(newPath, file->d_name);
-				
-				 lstat(file->d_name, &buf);
-				if(strcmp(file->d_name, "..") == 0 || strcmp(file->d_name, ".") == 0)
-				{
-					continue;
-				}
-				
-				//recursion if directory
-				if (S_ISDIR(buf.st_mode)) {
-					scanDIR(newPath, sign, date);
-				}
-			
-				// if file
-				if(S_ISREG(buf.st_mode)){
-					int operant = (int) sign;
-					switch(operant)
-					{
-						case '=':
-							if(difftime(buf.st_mtime, date) == 0){
-								getFileInfo(newPath,&buf);
-							}
-						break;
-						case '>':
-							if(difftime(buf.st_mtime, date) > 0){
-								getFileInfo(newPath,&buf);
-							}
-						break;
-						case '<':
-							if(difftime(buf.st_mtime, date) < 0)
-							{
-								getFileInfo(newPath,&buf);
-							}
-						break;
-						default:
-							printf("Error with operants");
-						break;
-						
-					}
-					
-					
-				}
-				  
+///DRUGA FUNKCJA W TAMTYM PLIKU
+int display_info(const char *fpath, const struct stat *fileStat, int tflag, struct FTW *ftwbuf){
+
+		if(tflag != FTW_F ){
+				return 0;
 		}
-		closedir(folder);	
-	}
-	else{
-		printf("error");
-	}	
-	
+
+    time_t date = fileStat->st_mtime;
+    char operant = nftwoperant[0];
+		switch(operant)
+		{
+			case '=':
+				if(difftime(date, nftwDate) == 0){
+					getFileInfo(fpath,fileStat);
+					return 0;
+				}
+			break;
+			case '>':
+				if(difftime(date, nftwDate) > 0){
+					getFileInfo(fpath,fileStat);
+					return 0;
+				}
+			break;
+			case '<':
+				if(difftime(date, nftwDate) < 0)
+				{
+					getFileInfo(fpath,fileStat);
+					return 0;
+				}
+			break;
+			default:
+					return 0;
+			break;
+
+		}
+
+			return 0;  // continue jak return 0
+
 }
 
-int main(int argc,char* argv[]) 
+
+int main(int argc,char* argv[])
 {
+	 // PROBLEM JEST TAKI ZE DATA MODYFIKACJI PLIKU SIE ROZNI TRZEBA TO OGARNAC JAKOS ALE TAK TO DZIALA
 	//char string[] = "a234"; // check for integer if == 0 than smth...
-	srand((unsigned int)time(NULL));
-	printf("%d",argc);
-	struct tm *timeptr;
+	struct tm *timeptr = malloc(sizeof(struct tm));
 	if(argc > 3)
 	{
-		timeptr = malloc(sizeof(struct tm));
-		strptime(argv[3], format, timeptr);
-		 time_t date = mktime(timeptr); // niby mamy date co robimy?
+		 //ROZWIAZANIE PROBLEMU DATY W 2 PLIKU
+		char *path = argv[1];
+    char *sign = argv[2];
+    char *userDate = argv[3];
+			strptime(argv[3], format, timeptr);
+		  time_t date = mktime(timeptr);
+	    printf("%s", ctime(&date));
+    nftwoperant = sign;
+		nftwDate = date;
+ 	nftw(realpath(path, NULL), display_info, 10, FTW_PHYS);
 		
-		//zmienic path
-		scanDIR(argv[1],argv[2][0], date);
-		
+
 	}else{
 		printf("Error, not enough arguments to program.");
 		exit(0);
-	}	
+	}
+
 	return 0;
+	free(timeptr);
 }
